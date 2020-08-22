1fbc7a314aa4f05d1f78ab91d999d83309975721
irrako <irraquex@gmail.com>
2020-08-22 14:01:28 -0400

    codigo_app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var serviceRouter = require('./routes/services');
 
 var app = express();
 
@@ -20,11 +20,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1.0/', serviceRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404)); 
 });
 
 // error handler
@@ -37,5 +39,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+var port = 8000;
+app.listen(port, () => {
+  console.log("Corriendo " + port);
+});
 module.exports = app;
